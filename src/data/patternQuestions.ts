8129355ce679a6664e8330f122c93f8fb7d135c9
tsukudakohei <kohei.tsukuda@bizlink.io>
2024-11-22 01:06:18 +0900

    fix

--- src/data/patternQuestions.ts
+++ src/data/patternQuestions.ts
@@ -3,96 +3,96 @@ import { PatternQuestion } from '@/types/questions';
 export const patternQuestions: PatternQuestion[] = [
   {
     id: 1,
-    questionImage: '/dist/images/1.png',
-    choicesImage: '/dist/images/2.png',
+    questionImage: 'https://vite-react-75pl7vco9-tsukuda-kouheis-projects.vercel.app/images/1.png',
+    choicesImage: 'https://vite-react-75pl7vco9-tsukuda-kouheis-projects.vercel.app/images/2.png',
     choices: ["A", "B", "C", "D", "E", "F"],
     answer: "F",
     type: 'pattern'
   },
   {
     id: 2,
-    questionImage: '/dist/images/3.png',
-    choicesImage: '/dist/images/4.png',
+    questionImage: 'https://vite-react-75pl7vco9-tsukuda-kouheis-projects.vercel.app/images/3.png',
+    choicesImage: 'https://vite-react-75pl7vco9-tsukuda-kouheis-projects.vercel.app/images/4.png',
     choices: ["A", "B", "C", "D", "E", "F"],
     answer: "A",
     type: 'pattern'
   },
   {
     id: 3,
-    questionImage: '/dist/images/5.png',
-    choicesImage: '/dist/images/6.png',
+    questionImage: 'https://vite-react-75pl7vco9-tsukuda-kouheis-projects.vercel.app/images/5.png',
+    choicesImage: 'https://vite-react-75pl7vco9-tsukuda-kouheis-projects.vercel.app/images/6.png',
     choices: ["A", "B", "C", "D", "E", "F"],
     answer: "F",
     type: 'pattern'
   },
   {
     id: 4,
-    questionImage: '/dist/images/7.png',
-    choicesImage: '/dist/images/8.png',
+    questionImage: 'https://vite-react-75pl7vco9-tsukuda-kouheis-projects.vercel.app/images/7.png',
+    choicesImage: 'https://vite-react-75pl7vco9-tsukuda-kouheis-projects.vercel.app/images/8.png',
     choices: ["A", "B", "C", "D", "E", "F"],
     answer: "A",
     type: 'pattern'
   },
   {
     id: 5,
-    questionImage: '/dist/images/9.png',
-    choicesImage: '/dist/images/10.png',
+    questionImage: 'https://vite-react-75pl7vco9-tsukuda-kouheis-projects.vercel.app/images/9.png',
+    choicesImage: 'https://vite-react-75pl7vco9-tsukuda-kouheis-projects.vercel.app/images/10.png',
     choices: ["A", "B", "C", "D", "E", "F"],
     answer: "B",
     type: 'pattern'
   },
   {
     id: 6,
-    questionImage: '/dist/images/11.png',
-    choicesImage: '/dist/images/12.png',
+    questionImage: 'https://vite-react-75pl7vco9-tsukuda-kouheis-projects.vercel.app/images/11.png',
+    choicesImage: 'https://vite-react-75pl7vco9-tsukuda-kouheis-projects.vercel.app/images/12.png',
     choices: ["A", "B", "C", "D", "E", "F"],
     answer: "B",
     type: 'pattern'
   },
   {
     id: 7,
-    questionImage: '/dist/images/13.png',
-    choicesImage: '/dist/images/14.png',
+    questionImage: 'https://vite-react-75pl7vco9-tsukuda-kouheis-projects.vercel.app/images/13.png',
+    choicesImage: 'https://vite-react-75pl7vco9-tsukuda-kouheis-projects.vercel.app/images/14.png',
     choices: ["A", "B", "C", "D", "E", "F"],
     answer: "F",
     type: 'pattern'
   },
   {
     id: 8,
-    questionImage: '/dist/images/15.png',
-    choicesImage: '/dist/images/16.png',
+    questionImage: 'https://vite-react-75pl7vco9-tsukuda-kouheis-projects.vercel.app/images/15.png',
+    choicesImage: 'https://vite-react-75pl7vco9-tsukuda-kouheis-projects.vercel.app/images/16.png',
     choices: ["A", "B", "C", "D", "E", "F"],
     answer: "F",
     type: 'pattern'
   },
   {
     id: 9,
-    questionImage: '/dist/images/17.png',
-    choicesImage: '/dist/images/18.png',
+    questionImage: 'https://vite-react-75pl7vco9-tsukuda-kouheis-projects.vercel.app/images/17.png',
+    choicesImage: 'https://vite-react-75pl7vco9-tsukuda-kouheis-projects.vercel.app/images/18.png',
     choices: ["A", "B", "C", "D", "E", "F"],
     answer: "F",
     type: 'pattern'
   },
   {
     id: 10,
-    questionImage: '/dist/images/19.png',
-    choicesImage: '/dist/images/20.png',
+    questionImage: 'https://vite-react-75pl7vco9-tsukuda-kouheis-projects.vercel.app/images/19.png',
+    choicesImage: 'https://vite-react-75pl7vco9-tsukuda-kouheis-projects.vercel.app/images/20.png',
     choices: ["A", "B", "C", "D", "E", "F"],
     answer: "C",
     type: 'pattern'
   },
   {
     id: 11,
-    questionImage: '/dist/images/21.png',
-    choicesImage: '/dist/images/22.png',
+    questionImage: 'https://vite-react-75pl7vco9-tsukuda-kouheis-projects.vercel.app/images/21.png',
+    choicesImage: 'https://vite-react-75pl7vco9-tsukuda-kouheis-projects.vercel.app/images/22.png',
     choices: ["A", "B", "C", "D", "E", "F"],
     answer: "B",
     type: 'pattern'
   },
   {
     id: 12,
-    questionImage: '/dist/images/23.png',
-    choicesImage: '/dist/images/24.png',
+    questionImage: 'https://vite-react-75pl7vco9-tsukuda-kouheis-projects.vercel.app/images/23.png',
+    choicesImage: 'https://vite-react-75pl7vco9-tsukuda-kouheis-projects.vercel.app/images/24.png',
     choices: ["A", "B", "C", "D", "E", "F"],
     answer: "D",
     type: 'pattern'
