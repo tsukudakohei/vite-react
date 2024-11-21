c88e400390bfb06e540fc4270499ecdb5e55e168
tsukudakohei <kohei.tsukuda@bizlink.io>
2024-11-22 00:46:21 +0900

    fix

--- src/data/patternQuestions.ts
+++ src/data/patternQuestions.ts
@@ -3,96 +3,96 @@ import { PatternQuestion } from '@/types/questions';
 export const patternQuestions: PatternQuestion[] = [
   {
     id: 1,
-    questionImage: '/images/1.png',
-    choicesImage: '/images/2.png',
+    questionImage: '/public/images/1.png',
+    choicesImage: '/public/images/2.png',
     choices: ["A", "B", "C", "D", "E", "F"],
     answer: "F",
     type: 'pattern'
   },
   {
     id: 2,
-    questionImage: '/images/3.png',
-    choicesImage: '/images/4.png',
+    questionImage: '/public/images/3.png',
+    choicesImage: '/public/images/4.png',
     choices: ["A", "B", "C", "D", "E", "F"],
     answer: "A",
     type: 'pattern'
   },
   {
     id: 3,
-    questionImage: '/images/5.png',
-    choicesImage: '/images/6.png',
+    questionImage: '/public/images/5.png',
+    choicesImage: '/public/images/6.png',
     choices: ["A", "B", "C", "D", "E", "F"],
     answer: "F",
     type: 'pattern'
   },
   {
     id: 4,
-    questionImage: '/images/7.png',
-    choicesImage: '/images/8.png',
+    questionImage: '/public/images/7.png',
+    choicesImage: '/public/images/8.png',
     choices: ["A", "B", "C", "D", "E", "F"],
     answer: "A",
     type: 'pattern'
   },
   {
     id: 5,
-    questionImage: '/images/9.png',
-    choicesImage: '/images/10.png',
+    questionImage: '/public/images/9.png',
+    choicesImage: '/public/images/10.png',
     choices: ["A", "B", "C", "D", "E", "F"],
     answer: "B",
     type: 'pattern'
   },
   {
     id: 6,
-    questionImage: '/images/11.png',
-    choicesImage: '/images/12.png',
+    questionImage: '/public/images/11.png',
+    choicesImage: '/public/images/12.png',
     choices: ["A", "B", "C", "D", "E", "F"],
     answer: "B",
     type: 'pattern'
   },
   {
     id: 7,
-    questionImage: '/images/13.png',
-    choicesImage: '/images/14.png',
+    questionImage: '/public/images/13.png',
+    choicesImage: '/public/images/14.png',
     choices: ["A", "B", "C", "D", "E", "F"],
     answer: "F",
     type: 'pattern'
   },
   {
     id: 8,
-    questionImage: '/images/15.png',
-    choicesImage: '/images/16.png',
+    questionImage: '/public/images/15.png',
+    choicesImage: '/public/images/16.png',
     choices: ["A", "B", "C", "D", "E", "F"],
     answer: "F",
     type: 'pattern'
   },
   {
     id: 9,
-    questionImage: '/images/17.png',
-    choicesImage: '/images/18.png',
+    questionImage: '/public/images/17.png',
+    choicesImage: '/public/images/18.png',
     choices: ["A", "B", "C", "D", "E", "F"],
     answer: "F",
     type: 'pattern'
   },
   {
     id: 10,
-    questionImage: '/images/19.png',
-    choicesImage: '/images/20.png',
+    questionImage: '/public/images/19.png',
+    choicesImage: '/public/images/20.png',
     choices: ["A", "B", "C", "D", "E", "F"],
     answer: "C",
     type: 'pattern'
   },
   {
     id: 11,
-    questionImage: '/images/21.png',
-    choicesImage: '/images/22.png',
+    questionImage: '/public/images/21.png',
+    choicesImage: '/public/images/22.png',
     choices: ["A", "B", "C", "D", "E", "F"],
     answer: "B",
     type: 'pattern'
   },
   {
     id: 12,
-    questionImage: '/images/23.png',
-    choicesImage: '/images/24.png',
+    questionImage: '/public/images/23.png',
+    choicesImage: '/public/images/24.png',
     choices: ["A", "B", "C", "D", "E", "F"],
     answer: "D",
     type: 'pattern'
